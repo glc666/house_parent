faefe687b2f953078030ecf62956cc71b82daaef
glc666 <2277088066@qq.com>
2019-04-13 21:30:51 +0800

    集成 freemark

--- house_web/src/main/java/com/house/controller/HelloController.java
+++ house_web/src/main/java/com/house/controller/HelloController.java
@@ -1,15 +1,20 @@
 package com.house.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @Author: ayuantalking
  * @Date: 2019/4/13/0013 09:41
  * @Version 1.0
  */
+@Controller
 public class HelloController {
-  
-    public String name = "jack";
 
-    private int age=18;
-
-    
+    @RequestMapping(value ="/hello")
+    public String hello(Model model){
+        model.addAttribute("message","你好jack");
+        return "hello";
+    }
 }
